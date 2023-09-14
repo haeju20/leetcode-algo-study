790d795a078643e23dc175ffca662907eda59294
haeju20 <1712wltn@gmail.com>
2023-09-14 21:33:46 +0900

    python Linked List

--- 21_merge_two_sorted_lists_sol.py
+++ 21_merge_two_sorted_lists_sol.py
@@ -1,7 +1,8 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # move smaller value to the left by swap
         if (not list1) or (list2 and list1.val > list2.val):
             list1, list2 = list2, list1
-        if list1:
+        if list1: # continue the swap list1.next
             list1.next = self.mergeTwoLists(list1.next, list2)
-        return list1 
+        return list1
