8b760d5c801cf180bbe8298d30c3cfe803e0ac39
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-09-25 14:45:41 +0900

    python Hash Table

--- 771_jewels_and_stones_sol.py
+++ 771_jewels_and_stones_sol.py
@@ -1,10 +1,40 @@
-class Solution: # using Counter
+class Solution: # using Hash Table
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         result = 0
-        cnt = collections.Counter(stones)
-        key = list(cnt.keys())
+        cnt = {}
+        # count the number of stones with comparison
+        for s in stones:
+            if s not in cnt:
+                cnt[s] = 1
+            else:
+                cnt[s] += 1
 
-        for i in jewels:
-            if i in key:
-                result += cnt[i]
+        for j in jewels:
+            if j in cnt:
+                result += cnt[j]
+
         return result
+
+# class Solution: # using defaultdict
+#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+#         result = 0
+#         cnt = collections.defaultdict(int)
+#         # count the number of stones
+#         for s in stones:
+#             cnt[s] += 1
+#
+#         for j in jewels:
+#             result += cnt[j]
+#
+#         return result
+
+# class Solution: # using Counter
+#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+#         result = 0
+#         cnt = collections.Counter(stones)
+#         key = list(cnt.keys())
+#
+#         for i in jewels:
+#             if i in key:
+#                 result += cnt[i]
+#         return result
