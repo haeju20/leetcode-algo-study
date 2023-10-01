a0db2e8a2f3daf3b7d316d6d28107d7652c96d92
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-10-01 11:35:13 +0900

    sql JOIN from programmers

--- SQL/review_list.sql
+++ SQL/review_list.sql
@@ -2,11 +2,15 @@
 -- print the list of reviews from members who have written the most reviews
 
 SELECT PROFILE.MEMBER_NAME, REVIEW.REVIEW_TEXT, DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-FROM MEMBER_PROFILE AS PROFILE, REST_REVIEW AS REVIEW
-WHERE PROFILE.MEMBER_ID = REVIEW.MEMBER_ID
-AND PROFILE.MEMBER_ID = (SELECT REVIEW.REVIEW_ID
-                         FROM REST_REVIEW AS REVIEW
-                        GROUP BY REVIEW.MEMBER_ID
-                        ORDER BY COUNT(MEMBER_ID) DESC
+FROM MEMBER_PROFILE AS PROFILE
+JOIN REST_REVIEW AS REVIEW
+ON PROFILE.MEMBER_ID = REVIEW.MEMBER_ID
+WHERE PROFILE.MEMBER_ID = (SELECT MEMBER_ID
+                         FROM REST_REVIEW
+                        GROUP BY MEMBER_ID
+                        ORDER BY COUNT(*) DESC
                         LIMIT 1)
 ORDER BY REVIEW_DATE, REVIEW_TEXT
+
+-- using SubQuery to find member who have written the most reviews
+-- using GROUP BY to group reviews by member_id
