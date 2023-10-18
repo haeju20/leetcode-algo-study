bcce0ca9f77173f1db9395924f72545d355409bb
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-10-18 18:35:49 +0900

    baekjoon 15652 - N과 M (4)

--- bj_15652.py
+++ bj_15652.py
@@ -19,3 +19,20 @@ def solution(N, M):
         p = p[:-1]
         
 solution(N, M)
+
+# N, M = list(map(int, input().split()))
+
+# p = []
+
+# def solution(depth, N, M):
+#     global p
+#     if M == 0:
+#         print(' '.join(p))
+#         return
+    
+#     for i in range(depth, N+1, 1):
+#         p.append(str(i))
+#         solution(i, N, M-1) # i <= i, i+1, ...
+#         p = p[:-1]
+        
+# solution(1, N, M)
