4fe77f8392149b348b5c88a6b1e9b3ae7041efc0
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-11-07 16:57:25 +0900

    baekjoon 1912 - 연속합

--- baekjoon/bj_1912.py
+++ baekjoon/bj_1912.py
@@ -13,3 +13,14 @@ for i in range(1, n): # 1, ..., n-1
     maxVal = max(maxVal, p[i])
     
 print(maxVal)
+
+# 틀린 코드
+# n = int(input())
+# p = list(map(int, input().split()))
+
+# dp = collections.defaultdict(int)
+# maxVal, dp[0] = p[0], p[0]
+
+# for i in range(1, n):
+#     dp[i] = max(dp[i-1]+p[i], p[i-1]+p[i])
+#     maxVal = max(maxVal, dp[i])
