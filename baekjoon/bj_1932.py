b31b8c32f7959fb69e884591fb9c5d2fdd11d651
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-11-08 14:49:17 +0900

    baekjoon 1932 - 정수 삼각형

--- baekjoon/bj_1932.py
+++ baekjoon/bj_1932.py
@@ -1,9 +1,35 @@
 # baekjoon 1932
 # 정수 삼각형
 
+import collections
 
+n = int(input())
+tri = [ list(map(int, input().split())) for _ in range(n) ]
+dp = collections.defaultdict(list)
+dp[0].append(tri[0][0]) # initial value
 
-# 틀린 코드
+for row in range(1, n): # row: 1, 2, ..., n-1
+    
+    if row == 1:
+        dp[row].append(dp[row-1][0] + tri[row][0])
+        dp[row].append(dp[row-1][0] + tri[row][1])
+        continue
+    
+    # row: 2, ..., n-1
+    for idx in range(row+1): # index of the row: 0, 1, ..., row
+        if idx == 0: # triangle[row-1][0] only 
+            dp[row].append(dp[row-1][idx] + tri[row][idx])
+        
+        elif idx == row: # triangle[row-1][row] only 
+            dp[row].append(dp[row-1][idx-1] + tri[row][idx])
+            
+        else: # except for the values of both ends
+            dp[row].append(max(dp[row-1][idx-1] + tri[row][idx], dp[row-1][idx] + tri[row][idx]))
+       
+print(max(dp[n-1]))
+
+# 틀린 코드 (시간 초과)
+# import sys
 # n = int(input())
 # triangle = [ list(map(int, input().split())) for _ in range(n) ]
 
