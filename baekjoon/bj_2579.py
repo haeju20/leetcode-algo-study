4ee744290ba123cc8d6c80bf4f8a2a459b441b43
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-11-11 21:43:37 +0900

    baekjoon 2579 - 계단 오르기

--- baekjoon/bj_2579.py
+++ baekjoon/bj_2579.py
@@ -1,6 +1,30 @@
 # baekjoon 2579
 # 계단 오르기
 
+import sys
+import collections
+
+n = int(input())
+score = [ int(input()) for _ in range(n) ]
+
+maxVal = -sys.maxsize
+dp = collections.defaultdict(int)
+dp[0] = score[0] # initial value
+
+for i in range(1, n):
+    if i == 1:
+        dp[i] = dp[i-1] + score[i]
+        continue
+    if i == 2:
+        dp[i] = max(dp[i-2]+score[i], score[i-1]+score[i]) 
+        continue
+ 
+    # i: 3, ..., n-1      
+    # 2칸 올라온 경우와 1칸 올라온(연속 세 계단 불가하므로 dp[-3]과 더함) 경우를 비교
+    dp[i] = max(dp[i-2]+score[i], dp[i-3]+score[i-1]+score[i]) 
+
+print(dp[n-1])
+
 # wrong answer
 # n = int(input())
 # score = [ int(input()) for _ in range(n) ]
