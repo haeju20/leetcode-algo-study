16aa77c6c4fbbd0310ee51ba23f80e8926d5536b
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-11-19 12:11:24 +0900

    Update swea_1859.py

--- swea_1859.py
+++ swea_1859.py
@@ -1,3 +1,17 @@
+# 출력 형식 바꾸면 정답
+def solution(p):
+    maxVal, cnt = p[-1], 0
+    for i in range(N-1, -1, -1):
+        maxVal = max(maxVal, p[i])
+        if maxVal > p[i]:
+            cnt += maxVal - p[i]
+    print(cnt)
+
+if __name__ == '__main__':
+    N = int(input())
+    p = list(map(int, input().split()))
+    solution(p)
+
 # fail (runtime error)
 # N = int(input())
 # p = list(map(int, input().split()))
