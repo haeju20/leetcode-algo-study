e349ceed7892a9c2415c51087f808dbf35813a28
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-12-02 21:43:42 +0900

    baekjoon 1149 - RGB거리

--- baekjoon/bj_1149.py
+++ baekjoon/bj_1149.py
@@ -11,7 +11,7 @@ for _ in range(N):
     p = list(map(int, input().split()))
     cost.append(p)
 
-for i in range(1, N): # value of (i)th row <- possible minimum value of (i-1)th row + (i)th row
+for i in range(1, N): # value of (i)th row <- the minimum possible value of (i-1)th row + (i)th row
     cost[i][0] = min(cost[i-1][1], cost[i-1][2]) + cost[i][0]
     cost[i][1] = min(cost[i-1][0], cost[i-1][2]) + cost[i][1]
     cost[i][2] = min(cost[i-1][0], cost[i-1][1]) + cost[i][2]
