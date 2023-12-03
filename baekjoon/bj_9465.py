fffad32a030340c0a38aa646fe6398bf8beab353
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-12-03 18:14:15 +0900

    baekjoon 9465 - 스티커

---
+++ baekjoon/bj_9465.py
@@ -0,0 +1,19 @@
+# baekjoon 9465
+# 스티커
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    score = [ list(map(int, input().split())) for _ in range(2) ] 
+
+    for i in range(1, n):
+        for j in range(2): # 0th row and 1st row
+            # initialize the value if 1st col
+            if i == 1:
+                score[j][i] += score[1-j][i-1] 
+                
+            # max value so far including current value 
+            score[j][i] += max(score[1-j][i-1], score[1-j][i-2])
+    
+    print(max(score[0][n-1], score[1][n-1]))
