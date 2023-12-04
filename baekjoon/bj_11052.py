b603f63a52c99402e83911e8d3d17e2d6e1da70a
JisuPark <53842434+haeju20@users.noreply.github.com>
2023-12-04 19:20:53 +0900

    baekjoon 11052 - 카드 구매하기

--- baekjoon/bj_11052.py
+++ baekjoon/bj_11052.py
@@ -1,22 +1,15 @@
 # baekjoon 11052
 # 카드 구매하기
 
-# wrong answer
-# import collections
+import collections
 
-# N = int(input())
-# p = list(map(int, input().split()))
-# dp = collections.defaultdict(int)
+N = int(input())
+p = list(map(int, input().split()))
+dp = collections.defaultdict(int)
 
-# # initialize
-# dp[N], dp[1] = p[N-1], p[0]*N
+for i in range(1, N+1): # 1, 2, ... N
+    dp[i] = p[i-1] # P as initial value
+    for j in range(1, i+1): # 1, 2, ..., i
+        dp[i] = max(dp[i], dp[i-j] + p[j-1]) 
 
-# for i in range(N-1, 1, -1): 
-#     if N % i == 0:
-#         dp[i] = (N//i) * p[i-1]
-#     elif i > N//2:
-#         dp[i] = p[i-1] + p[N-i-1]
-#     else:
-#         dp[i] = p[i-1] * (N//i) + N%i
-      
-# print(max(dp.values()))
+print(dp[N])
